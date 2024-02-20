83c1656204704f02293a25630837e2d8a913c2b9
captcha781 <bhuvanesh19112001@gmail.com>
2024-02-20 22:38:49 +0530

    Test

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        notojp: ['Noto Sans JP'],
+        roboto: ['Roboto']
+      }
     },
   },
   plugins: [],
